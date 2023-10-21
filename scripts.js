3fd50795ed57f1bda2e2f429384f766ebc2ab0a0
Abhishek Dhondge <43716694+4bhishek10@users.noreply.github.com>
2023-10-21 15:27:10 +0530

    BASE V0.1

---
+++ scripts.js
@@ -0,0 +1,48 @@
+// Replace these URLs with the paths to your videos
+const videoPaths = [
+    "instagram/insta2.mp4",
+    "instagram/insta1.mp4",
+    // "/home/abhishek/Downloads/Andrew Tate _ Official Trailer Movie (2023).mp4",
+    // Add more video paths here
+];
+
+let currentVideoIndex = 0;
+let playingVideo = null;
+
+function play_Next(){
+    console.log("clicked");
+    currentVideoIndex++;
+    loadAndPlayVideo(currentVideoIndex);
+
+}
+function play_Prev(){
+    console.log("clicked");
+    currentVideoIndex--;
+    loadAndPlayVideo(currentVideoIndex);
+}
+
+function loadAndPlayVideo(videoIndex) {
+    if (playingVideo) {
+        playingVideo.pause();
+    }
+
+    const video = $("#video-container video")[0];
+    const source = $("#video-container source");
+    if (videoIndex < videoPaths.length) {
+        source.prop("src", videoPaths[videoIndex]);
+        video.load();
+        video.play();
+        playingVideo = video;
+
+        video.onended = function () {
+            // Play the next video when the current video ends
+            currentVideoIndex++;
+            loadAndPlayVideo(currentVideoIndex);
+        };
+    }
+}
+
+$(document).ready(function () {
+    loadAndPlayVideo(currentVideoIndex);
+
+});
